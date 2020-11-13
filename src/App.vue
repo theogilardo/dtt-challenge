540dee75b05b1633b6457df97169af1bcc05b233
theogilardo <gilardotheo@gmail.com>
2020-11-13 22:11:19 +0100

    add footer

--- src/App.vue
+++ src/App.vue
@@ -4,17 +4,20 @@
     <!-- <transition name="slide-fade"> -->
     <router-view />
     <!-- </transition> -->
+    <the-footer></the-footer>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
 import TheNavbar from "./components/TheNavbar.vue";
+import TheFooter from "./components/TheFooter.vue";
 
 export default Vue.extend({
   name: "App",
   components: {
     TheNavbar,
+    TheFooter,
   },
 });
 </script>
---
+++ src/components/TheFooter.vue
@@ -0,0 +1,31 @@
+<template>
+  <footer>
+    <router-link to="/">Menu</router-link>
+    <router-link to="/categories">Categories</router-link>
+    <router-link to="/random">Random Recipe</router-link>
+  </footer>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({});
+</script>
+
+<style lang="stylus" scoped>
+
+footer
+  position fixed
+  bottom 0
+  width 100%
+  height 3.5rem
+  background #33333380
+  color white
+  display flex
+  align-items center
+  justify-content space-around
+
+  & > *
+    color white
+    font-weight bold
+    text-transform uppercase
+</style>
